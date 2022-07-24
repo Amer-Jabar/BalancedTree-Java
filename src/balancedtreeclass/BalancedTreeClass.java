5fff97912f08cd9873c544b176ae11e477685498
Amer-Jabar <amerjabar0000@gmail.com>
2022-07-24 15:50:59 +0300

    Add metric for measuring time complexity.

--- src/balancedtreeclass/BalancedTreeClass.java
+++ src/balancedtreeclass/BalancedTreeClass.java
@@ -2,6 +2,8 @@ package balancedtreeclass;
 
 public class BalancedTreeClass {
     
+    // Insertion is in linear time.
+    
     public static void main(String[] args) {
         
         BalancedTree t = new BalancedTree();
@@ -12,8 +14,12 @@ public class BalancedTreeClass {
         t.insert(t.root, 3);
         t.insert(t.root, 4);
         t.insert(t.root, -5);
-        t.insert(t.root, 0);
-        t.print(t.root);
+        t.insert(t.root, -6);
+        t.insert(t.root, -7);
+        t.clearComplexity();
+        t.insert(t.root, -2);
+        t.printComplexity();
+        
         
     }
     
@@ -22,16 +28,20 @@ public class BalancedTreeClass {
 class BalancedTree {
     
     Node root;
+    int complexity;
     
     public BalancedTree() {
         root = new Node();
+        complexity = 0;
     }
     
     public BalancedTree(Integer data) {
         root = new Node(data);
+        complexity = 0;
     }
     
     public void insert(Node node, Integer data) {
+        complexity++;
         if ( node.data == null ) {
             node.data = data;
             return;
@@ -57,6 +67,15 @@ class BalancedTree {
         if ( node.right != null )
             print(node.right);
     }
+    
+    public void printComplexity() {
+        System.err.println("The complexity yet has been " + complexity);
+    }
+    
+    public void clearComplexity() {
+        complexity = 0;
+    }
+    
 }
 
 class Node {
